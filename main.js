7a05d9832afdd6a8bfa545dd54c101931363576a
Michael Ion <michael.ion.mi@outlook.com>
2019-08-31 20:28:50 -0400

    announce cards

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const suits     = ["Clubs", "Diamonds", "Hearts", "Spades"];
 const deck      = [];
 let player1Card = null;
 let player2Card = null;
+const p1Score = 0;
+const p2Score = 0;
 
 
 function buildDeck() {
@@ -32,13 +34,25 @@ function dealCardsToPlayers() {
 let newArray = arrayShuffle(deck);
 
 newArray.forEach((item, idx)=>{
-  player1Card = newArray.splice(idx,1)
-  console.log(player1Card.length);
-  console.log(newArray.length);
-  if(newArray.length <= 26){
-    console.log(`out of cards`);
-  }
+  player1Card = newArray.splice(idx,1);
+  // console.log(player1Card.length);
+  // console.log(newArray.length);
+  // if(newArray.length <= 26){
+  //   console.log(`out of cards`);
+  // }
+  return player1Card;
 })
+
+newArray.forEach((item, idx)=>{
+  player2Card = newArray.splice(idx,1);
+  return player2Card;
+  // console.log(player2Card);
+})
+
+// player1Card.forEach((item)=>{
+//   console.log(`player1 dealt ${item}`)
+
+// })
 
 // player1Card.forEach((item)=>{
 //   console.log(`player 1 dealt card ${player1Card}`);
@@ -49,6 +63,16 @@ newArray.forEach((item, idx)=>{
 }
 
 function announceCards() {
+
+  player1Card.forEach((dealt)=>{
+  console.log(`player 1 dealt ${dealt}`)
+  console.log(player1Card.length)
+})
+  player2Card.forEach((dealt)=>{
+  console.log(`player 2 dealt ${dealt}`)
+  console.log(player2Card.length)
+})
+
 }
 
 function cardToRank(card) {
